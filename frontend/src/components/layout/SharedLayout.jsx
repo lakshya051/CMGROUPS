bc5e276ecc064b34f88bc81d4de5456acf925625
lakshya051 <lakshyavarshney20@gmail.com>
2026-03-06 18:53:16 +0530

    search fixed for mobile

--- frontend/src/components/layout/SharedLayout.jsx
+++ frontend/src/components/layout/SharedLayout.jsx
@@ -7,7 +7,7 @@ const SharedLayout = () => {
     return (
         <div className="min-h-screen flex flex-col bg-background text-text-main">
             <Navbar />
-            <main className="flex-grow pt-16">
+            <main className="flex-grow pt-32 md:pt-16">
                 <Outlet />
             </main>
             <Footer />
